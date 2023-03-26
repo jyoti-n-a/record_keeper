2a8e74928415483d4a99e3852a99f40d90ba8001
jyotinair <jyotinair31@gmail.com>
2023-03-26 15:49:58 +0800

    New Fragment fro Cycling

--- app/src/main/java/com/learning/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/learning/recordkeeper/MainActivity.kt
@@ -2,10 +2,46 @@ package com.learning.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.commit
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.learning.recordkeeper.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       binding.buttonRunning.setOnClickListener() {
+           onRunningClicked()
+       }
+
+        binding.buttonCycling.setOnClickListener() {
+            onCyclingClicked()
+        }
+
+
+    }
+
+    private fun onCyclingClicked() {
+        // this.FragmentTransaction help to switch between the fragment
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+
+    }
+
+    private fun onRunningClicked() {
+        // this.FragmentTransaction help to switch between the fragment
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, RunningFragment())
+        }
+
     }
 }
